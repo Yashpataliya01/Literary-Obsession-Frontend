65818ac95c70ce4830a2f5a990b6cf76d491c8be
yashpataliya01 <yashpataliya01@gmail.com>
2024-08-19 23:56:32 +0530

    first commit

--- src/Context/Bookdata.jsx
+++ src/Context/Bookdata.jsx
@@ -3,7 +3,6 @@ import React, { createContext, useState, useEffect } from "react";
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
   const [Classic, setClassic] = useState([]);
   const [bestseller, setbestseller] = useState([]);
   const [categoryselection, setcategoryselection] = useState("Romance");
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
@@ -7,8 +7,9 @@ export default defineConfig({
       '/api': {
         target: 'https://literary-obsession-backend.vercel.app',
         secure: false,
+        changeOrigin: true,
       },
     },
   },
   plugins: [react()],
-})
+});
