9f3c0f574c36e858ba224c17228ba52f9874907a
yashpataliya01 <yashpataliya01@gmail.com>
2024-08-20 15:36:33 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import styles from "./Home.module.css";
+import homeimg from "../../public/home.png";
 import Bestseller from "./Bestseller";
 import Oldandrare from "./Oldandrare";
 import { AppContext } from "../Context/Bookdata";
@@ -23,7 +24,7 @@ function Home() {
 
   return (
     <>
-      <img src="/home.png" alt="Home" className={styles.homeimg} />
+      <img src={homeimg} alt="Home" className={styles.homeimg} />
       <div className={styles.home}>
         <div className={styles.content}>
           <div className={styles.heading}>
