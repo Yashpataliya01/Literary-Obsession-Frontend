278da3499bed8ef6fa0bc637e37fed2ae5103590
yashpataliya01 <yashpataliya01@gmail.com>
2024-08-22 16:33:55 +0530

    first commit

--- src/components/Oldandrare.jsx
+++ src/components/Oldandrare.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -9,7 +9,6 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { AppContext } from "../Context/Bookdata";
 import styles from "./Oldandrare.module.css";
-
 function Oldandrare({ booktitle, name, stylees }) {
   const token = localStorage.getItem("token");
   const apiUrl =
@@ -18,8 +17,6 @@ function Oldandrare({ booktitle, name, stylees }) {
       : "/api";
   const { favcount, setFavcount } = useContext(AppContext);
   const navigate = useNavigate();
-  const [userFav, setUserFav] = useState(false); // Corrected useState
-
   useGSAP(() => {
     gsap.from(`.${styles.mySwiper}`, {
       opacity: 0,
@@ -35,7 +32,7 @@ function Oldandrare({ booktitle, name, stylees }) {
     });
   });
 
-  const addtofav = async (bookid) => {
+  const addtofav = async (bookid, e) => {
     if (localStorage.getItem("isLogin") === "true") {
       try {
         const response = await fetch(`${apiUrl}/function/addfav`, {
@@ -50,9 +47,8 @@ function Oldandrare({ booktitle, name, stylees }) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        await response.json();
+        const res = await response.json();
         setFavcount(favcount + 1);
-        setUserFav(true); // Set userFav to true after adding
       } catch (error) {
         console.error("Error adding to favorites:", error);
       }
@@ -61,7 +57,6 @@ function Oldandrare({ booktitle, name, stylees }) {
       navigate("/signin");
     }
   };
-
   const removefav = async (bookid) => {
     if (localStorage.getItem("isLogin") === "true") {
       try {
@@ -76,11 +71,10 @@ function Oldandrare({ booktitle, name, stylees }) {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-        await response.json();
+        const res = await response.json();
         setFavcount(favcount - 1);
-        setUserFav(false); // Set userFav to false after removing
       } catch (error) {
-        console.error("Error removing from favorites:", error);
+        console.error("Error removing to favorites:", error);
       }
     } else {
       signout();
@@ -96,108 +90,86 @@ function Oldandrare({ booktitle, name, stylees }) {
       } else {
         localStorage.clear();
         navigate("/");
+        setIslogin(false);
         setFavcount(0);
       }
-    } catch (error) {
-      console.error("Sign out error:", error);
+    } catch {
       navigate("/signin");
     }
   };
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch(`${apiUrl}/auth/getuser`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const userData = await response.json();
-        const userFavs = new Set(userData.fav || []);
-        if (booktitle.some((book) => userFavs.has(book._id))) {
-          setUserFav(true);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    if (token) {
-      fetchUserData();
-    }
-  }, [apiUrl, token, booktitle]);
-
   return (
-    <div className={styles.oldandrare}>
-      <h1 className={styles.heading} style={{ display: stylees }}>
-        {name}
-      </h1>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={false}
-        centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        modules={[EffectCoverflow, Pagination, Autoplay]}
-        className={styles.mySwiper}
-      >
-        {booktitle.map((book) => (
-          <SwiperSlide className={styles.swiperslide} key={book._id}>
-            <Link
-              to={{ pathname: `/book/${book.title}` }}
-              state={{ ...book, booktitle }}
-            >
-              <img src={book.image} alt={book.title} className={styles.img} />
-            </Link>
-            <div className={styles.bookdetails}>
-              <h1>{book.title.substring(0, 31)}</h1>
-              <p>{book.author}</p>
-              <div className={styles.innerdiv}>
-                <h2>₹{book.price} /-</h2>
-                {userFav ? (
-                  <button
-                    className={styles.favButton}
-                    onClick={() => removefav(book._id)}
-                  >
-                    <i
-                      className="fa-solid fa-heart"
-                      style={{ color: "red" }}
-                    ></i>
-                  </button>
-                ) : (
-                  <button
-                    className={styles.favButton}
-                    onClick={() => addtofav(book._id)}
-                  >
-                    <i className="fa-regular fa-heart"></i>
-                  </button>
-                )}
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <button className={styles.button}>
-        <Link
-          className={styles.link}
-          to={{ pathname: "/books" }}
-          state={booktitle}
+    <>
+      <div className={styles.oldandrare}>
+        <h1 className={styles.heading} style={{ display: stylees }}>
+          {name}
+        </h1>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={false}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectCoverflow, Pagination, Autoplay]}
+          className={styles.mySwiper}
         >
-          View All
-        </Link>
-      </button>
-    </div>
+          {booktitle.map((book) => (
+            <SwiperSlide className={styles.swiperslide} key={book.title}>
+              <Link
+                to={{ pathname: `/book/${book.title} ` }}
+                state={{ ...book, booktitle }}
+              >
+                <img src={book.image} alt={book.title} className={styles.img} />
+              </Link>
+              <div className={styles.bookdetails}>
+                <h1>{book.title.substring(0, 31)}</h1>
+                <p>{book.author}</p>
+                <div className={styles.innerdiv}>
+                  <h2>₹{book.price} /-</h2>
+                  {book.fav ? (
+                    <button
+                      className={styles.favButton}
+                      onClick={(e) => removefav(book._id)}
+                    >
+                      <i
+                        className="fa-solid fa-heart"
+                        style={{ color: "red" }}
+                      ></i>
+                    </button>
+                  ) : (
+                    <button
+                      className={styles.favButton}
+                      onClick={(e) => addtofav(book._id, e)}
+                    >
+                      <i className="fa-regular fa-heart"></i>
+                    </button>
+                  )}
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button className={styles.button}>
+          <Link
+            className={styles.link}
+            to={{ pathname: "/books" }}
+            state={booktitle}
+          >
+            View All
+          </Link>
+        </button>
+      </div>
+    </>
   );
 }
 
